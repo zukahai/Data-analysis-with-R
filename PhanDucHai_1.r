a7b7e259b0a465454f9d9cda14aecfae766c4314
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-08-24 09:41:29 +0700

    B1

---
+++ PhanDucHai_1.r
@@ -0,0 +1,41 @@
+#Duong dan lib
+.libPaths()
+
+#thu vien
+library()
+
+#Tim kiem
+search()
+
+#cai package
+#install.packages("XML")
+
+
+#load package
+library("XML")
+
+a <- "Hello world"
+print(a)
+
+
+###############
+
+name <- "Hai"
+age <- 22
+cat("My name is", name, "\nAge: ", age, "\n")
+cat(class(name), "\n", class(age))
+
+#==========================
+#loop in R
+for (year in 2016:2022){
+  print(paste("The year is", year))
+}
+for (year in 2022:2016){
+  print(paste("The year is", year))
+}
+
+year <- 1
+while (year < 6) {
+  print(year)
+  year <- year + 1
+}
