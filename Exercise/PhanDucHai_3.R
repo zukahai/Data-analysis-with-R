e9a540dcba57e4f7fb40e9b4dfcee29ba311a16c
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-09-06 19:33:21 +0700

    Create PhanDucHai_3.R

---
+++ Exercise/PhanDucHai_3.R
@@ -0,0 +1,5 @@
+my.name <- readline(prompt="Enter name: ")
+my.age <- readline(prompt="Enter age: ")
+# convert character into integer
+my.age <- as.integer(my.age)
+print(paste("Hi,", my.name, "next year you will be", my.age+1, "years old."))
